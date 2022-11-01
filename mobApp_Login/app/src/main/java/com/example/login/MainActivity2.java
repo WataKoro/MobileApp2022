bc1343e23d56d4c913d50d966c28832cf0569540
triantisna <triantisna17@gmail.com>
2022-11-01 14:22:31 +0700

    Login

--- mobApp_Login/app/src/main/java/com/example/login/MainActivity2.java
+++ mobApp_Login/app/src/main/java/com/example/login/MainActivity2.java
@@ -8,21 +8,21 @@ import android.view.MenuItem;
 import android.view.View;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
 
-    public void wali(View v){
-        Intent switchActivityIntent = new Intent(this, MainActivity2.class);
+    public void lansia(View v){
+        Intent switchActivityIntent = new Intent(this, MainActivity.class);
         startActivity(switchActivityIntent);
     }
 
-    public void wali(MenuItem item){
-        Intent switchActivityIntent = new Intent(this, MainActivity2.class);
+    public void lansia(MenuItem item){
+        Intent switchActivityIntent = new Intent(this, MainActivity.class);
         startActivity(switchActivityIntent);
     }
 }
