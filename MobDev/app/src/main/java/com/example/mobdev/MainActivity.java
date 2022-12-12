f2b0df82fb1e60fcbf57f17bb0508d4316c576eb
IrfanEsa <kadalseptimvii@gmail.com>
2022-12-12 17:22:28 +0700

    Get data collection, sensor dan notifikasi elder

--- MobDev/app/src/main/java/com/example/mobdev/MainActivity.java
+++ MobDev/app/src/main/java/com/example/mobdev/MainActivity.java
@@ -2,30 +2,66 @@ package com.example.mobdev;
 
 import static android.app.PendingIntent.getActivity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.messaging.FirebaseMessaging;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private FirebaseUser firebaseUser;
     private TextView AccountName;
+
+    private TextView tvHasil;
+    private SensorManager sm;
+    private Sensor senAccel;
+    String TAG = "debug_kel6";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AccountName = findViewById(R.id.AccountName);
+        //generate token untuk device
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+                        Log.d("token", token);
+                    }
+                });
+
+        tvHasil = findViewById(R.id.tvHasil);
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
@@ -34,7 +70,71 @@ public class MainActivity extends AppCompatActivity {
         }else{
             AccountName.setText("Login Gagal!");
         }
+
+        Bundle extras = getIntent().getExtras();
+        String userName;
+
+        if (extras != null) {
+            String nama = extras.getString("nama");
+            String umur = extras.getString("umur");
+
+            Log.d(TAG,nama + ":" + umur);
+        }
+
+
+        sm = (SensorManager) getSystemService(getApplicationContext().SENSOR_SERVICE);
+        senAccel = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (sm != null && senAccel != null){
+            Log.d(TAG,"Sukses, device punya sensor accelerometer!");
+        }
+        else {
+            // gagal, tidak ada sensor accelerometer.
+            Log.d(TAG,"Tidak ada sensor accelerometer!");
+        }
+
     }
+
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        double ax=0,ay=0,az=0;
+        boolean isTabrakan = false;
+        // menangkap perubahan nilai sensor
+        if (sensorEvent.sensor.getType()==Sensor.TYPE_ACCELEROMETER) {
+            ax=sensorEvent.values[0];
+            ay=sensorEvent.values[1];
+            az=sensorEvent.values[2];
+        }
+        if  (az<=-11) {
+            isTabrakan = true;
+        }
+        if (isTabrakan) {
+            Log.d(TAG, "TABRAKAN!!");
+            tvHasil.setText("TABRAKAN!!");
+        } else{
+            long timestamp = System.currentTimeMillis();
+            // Menampilkan log dari accelerometer beserta timestamp
+            String msg = "X: " + ax + ", Y: " + ay + ", Z: " + az + ", Timestamp: " + timestamp;
+            Log.d("Position", msg);
+            tvHasil.setText(msg);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sm.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sm.registerListener(this, senAccel, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
 
     public void nextActivity(View v){
         Intent switchActivityIntent = new Intent(this, MainActivity2.class);
--- MobDev/app/src/main/java/com/example/mobdev/MainActivity2.java
+++ MobDev/app/src/main/java/com/example/mobdev/MainActivity2.java
@@ -1,22 +1,35 @@
 package com.example.mobdev;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Map;
 
 public class MainActivity2 extends AppCompatActivity {
 
     private FirebaseUser firebaseUser;
-    private TextView Name, Email;
+    FirebaseFirestore db;
+    private TextView Name, Email, Tanggal, Phone, Alamat;
     private Button btnLogout;
+    String Tanggaltmp, Phonetmp, Alamattmp;
+
+    private Map<String,Object> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +37,47 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         Name = findViewById(R.id.name);
         Email = findViewById(R.id.email);
+        Tanggal = findViewById(R.id.tanggal);
+        Phone = findViewById(R.id.etphone);
+        Alamat = findViewById(R.id.alamat);
         btnLogout = findViewById(R.id.logout);
+        String TAG = "debug_kel6";
+        Log.d(TAG,"Load semua data");
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        db = FirebaseFirestore.getInstance();
+
+        btnLogout.setOnClickListener(v ->{
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(getApplicationContext(), Login.class));
+            finish();
+        });
+
+        db.collection("user")
+                .whereEqualTo("Mail", firebaseUser.getEmail())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                data = document.getData();
+                                Tanggaltmp = String.valueOf(data.get("Tanggal"));
+                                Tanggal.setText(Tanggaltmp);
+
+                                Alamattmp = String.valueOf(data.get("Alamat"));
+                                Alamat.setText(Alamattmp);
+
+                                Phonetmp = String.valueOf(data.get("Telepon"));
+                                Phone.setText(Phonetmp);
+                                Log.d(TAG, document.getId() + " => " + data);
+                                Log.d(TAG, data.get("Mail") + " => " + Tanggaltmp);
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
 
         if (firebaseUser!=null){
             Name.setText(firebaseUser.getDisplayName());
@@ -35,11 +86,6 @@ public class MainActivity2 extends AppCompatActivity {
             Name.setText("Login Gagal!");
         }
 
-        btnLogout.setOnClickListener(v ->{
-            FirebaseAuth.getInstance().signOut();
-            startActivity(new Intent(getApplicationContext(), Login.class));
-            finish();
-        });
     }
 
     public void logout(View v){
